bee6ab8d862f4554dcdfa26fbe10e0613e019289
mahitha09 <mahitha.yellani@gmail.com>
2021-05-26 15:45:39 +0530

    Update

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-const textArea = document.querySelector(".textArea");
-const btnTranslate = document.querySelector(".btn-translate");
-const divOutput = document.querySelector(".div-output");
+let textArea = document.getElementById('textArea');
+let btnTranslate = document.getElementById('btn');
+let output = document.getElementById('div-output');
 
+btnTranslate.addEventListener('click',()=>{
+    let txtInput = textArea.value;
+    let url = "https://api.funtranslations.com/translate/minion.json" + "?"+"text="+txtInput;
+
+    fetch(url)
+    .then(response => response.json())
+    .then(json => {
+        let translatedTxt = json.contents.translated;
+        console.log(translatedTxt);
+        output.classList.add('output');
+        output.textContent = translatedTxt;
+
+    });
+});
